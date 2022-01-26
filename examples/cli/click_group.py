c2bb91c4eef0fe578857c83144b142726f47fff8
gmolveau <2090495+gmolveau@users.noreply.github.com>
2022-01-26 13:24:39 +0100

    update cli examples

--- examples/cli/click_group.py
+++ examples/cli/click_group.py
@@ -1,6 +1,11 @@
 import click
 
 
+def positive_int(ctx, param, value):
+    if value < 0:
+        raise click.BadParameter("Should be a positive integer")
+    return value
+
 @click.group()
 def root():
     pass
@@ -14,8 +19,10 @@ def cmd():
 @cmd.command()
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', required=True, help='The person to greet.')
-def hello(count, name):
+@click.argument('n', type=int, default=1, callback=positive_int)
+def hello(count, name, n):
     """Simple program that greets NAME for a total of COUNT times."""
+    print(f"{n=}")
     for x in range(count):
         click.echo(f"Hello {name}!")
 
